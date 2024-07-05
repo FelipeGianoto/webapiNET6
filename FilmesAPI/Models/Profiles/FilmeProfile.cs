6b96842fcaaee2eef9089c27665081af579bb049
FelipeGianoto <felipe.gianoto1234@gmail.com>
2024-07-05 01:08:52 -0300

    First commit

---
+++ FilmesAPI/Models/Profiles/FilmeProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using FilmesAPI.Data.Dtos;
+
+namespace FilmesAPI.Models.Profiles;
+
+public class FilmeProfile : Profile
+{
+    public FilmeProfile()
+    {
+        CreateMap<CreateFilmeDto, Filme>();
+        CreateMap<UpdateFilmeDto, Filme>();
+        CreateMap<Filme, UpdateFilmeDto>();
+        CreateMap<Filme, ReadFilmeDto>();
+    }
+}
